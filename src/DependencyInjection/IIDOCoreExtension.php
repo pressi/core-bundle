7ebeaba00552eed1f862d1ea480e33ea8ebdec2d
pressi <mail@stephanpressl.at>
2021-01-12 10:29:36 +0100

    Create IIDOCoreExtension.php

---
+++ src/DependencyInjection/IIDOCoreExtension.php
@@ -0,0 +1,65 @@
+<?php
+declare(strict_types=1);
+
+/*******************************************************************
+ * (c) 2020 Stephan Preßl, www.prestep.at <development@prestep.at>
+ * All rights reserved
+ * Modification, distribution or any other action on or with
+ * this file is permitted unless explicitly granted by IIDO
+ * www.iido.at <development@iido.at>
+ *******************************************************************/
+
+namespace IIDO\CoreBundle\DependencyInjection;
+
+
+use Symfony\Component\Config\FileLocator;
+//use Symfony\Component\HttpKernel\DependencyInjection\Extension;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Extension\PrependExtensionInterface;
+use Symfony\Component\DependencyInjection\Extension\Extension;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
+
+
+/**
+ * Configures the Contao IIDO Core Bundle.
+ *
+ * @author Stephan Preßl <https://github.com/pressi>
+ */
+class IIDOCoreExtension extends Extension implements PrependExtensionInterface
+{
+    /**
+     * {@inheritdoc}
+     *
+     * @throws \Exception
+     */
+    public function load(array $configs, ContainerBuilder $container): void
+    {
+        $loader = new YamlFileLoader( $container, new FileLocator(__DIR__ . '/../Resources/config') );
+
+//        $loader->load('listener.yml');
+        $loader->load('services.yml');
+    }
+
+
+    /**
+     * {@inheritdoc}
+     *
+     * @throws \Exception
+     */
+    public function prepend(ContainerBuilder $container): void
+    {
+        $rootDir = $container->getParameter('kernel.root_dir');
+
+        if (file_exists($rootDir . '/config/parameters.yml') || !file_exists($rootDir . '/config/parameters.yml.dist'))
+        {
+            return;
+        }
+
+        $loader = new YamlFileLoader(
+            $container,
+            new FileLocator($rootDir . '/config')
+        );
+
+        $loader->load('parameters.yml.dist');
+    }
+}
