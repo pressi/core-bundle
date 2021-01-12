cf0ac0b4c7ebd4c8b7dc3758b67ea1c08e58695d
pressi <mail@stephanpressl.at>
2021-01-12 10:29:33 +0100

    Create IIDOCoreBundleTest.php

---
+++ tests/IIDOCoreBundleTest.php
@@ -0,0 +1,30 @@
+<?php
+/*******************************************************************
+ * (c) 2020 Stephan Preßl, www.prestep.at <development@prestep.at>
+ * All rights reserved
+ * Modification, distribution or any other action on or with
+ * this file is permitted unless explicitly granted by IIDO
+ * www.iido.at <development@iido.at>
+ *******************************************************************/
+
+namespace IIDO\CoreBundle\Tests;
+
+
+use IIDO\CoreBundle\IIDOCoreBundle;
+use PHPUnit\Framework\TestCase;
+
+
+/**
+ * Test the Contao IIDO Core Bundle.
+ *
+ * @author Stephan Preßl <development@prestep.at>
+ */
+class IIDOCoreBundleTest extends TestCase
+{
+    public function testCanBeInstantiated()
+    {
+        $bundle = new IIDOCoreBundle();
+
+        $this->assertInstanceOf('IIDO\CoreBundle\IIDOCoreBundle', $bundle);
+    }
+}
