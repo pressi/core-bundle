092cece4390bf5b6013e052e4e975f36f2eb1103
pressi <mail@stephanpressl.at>
2021-04-05 13:14:22 +0200

    Update TwigTemplatesExtension.php

--- src/Twig/TwigTemplatesExtension.php
+++ src/Twig/TwigTemplatesExtension.php
@@ -12,7 +12,9 @@ namespace IIDO\CoreBundle\Twig;
 
 use Contao\Controller;
 use Contao\StringUtil;
+use Contao\Validator;
 use IIDO\BasicBundle\Helper\BasicHelper;
+use IIDO\CoreBundle\Config\BundleConfig;
 use Twig\Extension\AbstractExtension;
 use Twig\TwigFilter;
 use Twig\TwigFunction;
@@ -29,7 +31,10 @@ class TwigTemplatesExtension extends AbstractExtension
     {
         return array
         (
-            new TwigFilter('language', array($this, 'languageFilter')),
+            new TwigFilter('language', [$this, 'languageFilter']),
+            new TwigFilter('renderId', [$this, 'renderIdFilter']),
+            new TwigFilter('renderScriptVar', [$this, 'renderScriptVarFilter']),
+            new TwigFilter('checkUUID', [$this, 'checkdUUIDFilter'])
 //            new TwigFilter('price', array($this, 'priceFilter')),
 //            new TwigFilter('replaceNL', array($this, 'replaceNewLineFilter')),
 //            new TwigFilter('masterStylesheetExists', array($this, 'checkIfMasterStylesheetExists')),
@@ -39,15 +44,19 @@ class TwigTemplatesExtension extends AbstractExtension
 
 
 
-//    /**
-//     * get Twig Template Functions
-//     *
-//     * @return array|TwigFunction[]
-//     */
-//    public function getFunctions()
-//    {
-//        return array
-//        (
+    /**
+     * get Twig Template Functions
+     *
+     * @return array|TwigFunction[]
+     */
+    public function getFunctions()
+    {
+        return array
+        (
+            new TwigFunction('websiteIcon', [$this, 'getWebsiteIcon'], ['is_safe' => ['html']]),
+
+            new TwigFunction('getLayout', [$this, 'getLayout'], ['is_safe' => ['html']]),
+            new TwigFunction('renderLayoutChooser', [$this, 'renderLayoutChooser'], ['is_safe' => ['html']])
 //            new TwigFunction('websiteTitle', array($this, 'getWebsiteTitleFunction')),
 //            new TwigFunction('socialmedia', array($this, 'getSocialmediaFunction')),
 //            new TwigFunction('ua', array($this, 'getUAFunctions')),
@@ -56,8 +65,8 @@ class TwigTemplatesExtension extends AbstractExtension
 //            new TwigFunction('getTrans', [$this, 'getTrans']),
 //            new TwigFunction('renderClass', [$this, 'renderClass']),
 //            new TwigFunction('generateRoute', [$this, 'generateRoute'])
-//        );
-//    }
+        );
+    }
 
 
 
@@ -80,6 +89,106 @@ class TwigTemplatesExtension extends AbstractExtension
         }
 
         return '';
+    }
+
+
+
+    public function checkdUUIDFilter( $uuidBinary )
+    {
+        if( Validator::isBinaryUuid( $uuidBinary ) )
+        {
+            $uuidBinary = StringUtil::binToUuid( $uuidBinary );
+        }
+
+        return $uuidBinary;
+    }
+
+
+
+    public function renderIdFilter( $id )
+    {
+        if( preg_match('/\_/', $id) )
+        {
+            $parts = StringUtil::trimsplit('_', $id);
+            $id = implode('-', $parts);
+        }
+        else
+        {
+            preg_match_all('/((?:^|[A-Z])[a-z]+)/', $id,$matches);
+
+            if( is_array($matches) && count($matches[0]) )
+            {
+                $id = implode('-', $matches[0]);
+            }
+        }
+
+        return $id;
+    }
+
+
+
+    public function renderScriptVarFilter( $variable )
+    {
+        if( preg_match('/\_/', $variable) )
+        {
+            $parts = StringUtil::trimsplit('_', $variable);
+
+            $variable = '';
+            foreach( $parts as $part )
+            {
+                $variable .= ucfirst( $part );
+            }
+        }
+
+        return ucfirst( $variable );
+    }
+
+
+
+    public function getWebsiteIcon( $icon )
+    {
+        return file_get_contents( BundleConfig::getRootDir( true ) . BundleConfig::getBundlePath( true ). '/images/icons/' . $icon );
+    }
+
+
+
+    public function getLayout( $name, $value )
+    {
+        Controller::loadLanguageFile('iido');
+
+        $bundlePath = BundleConfig::getBundlePath( true, false );
+        $content = '<div class="layout-container">';
+
+        if( !$value )
+        {
+            $value = 'layout01';
+        }
+
+        $content .= '<div class="layout-image"><img src="' . $bundlePath . '/images/layouts/header/' . $value . '.png"></div>';
+        $content .= '<div class="layout-label"><span>' . $GLOBALS['TL_LANG']['IIDO']['layouts']['header'][ $value ] . '</span></div>';
+
+        return $content . '</div>';
+    }
+
+
+
+    public function renderLayoutChooser( $name, $label, $value )
+    {
+        if( !$value )
+        {
+            $value = 'layout01';
+        }
+
+        $bundlePath = BundleConfig::getBundlePath( true, false );
+
+        $content = '<div class="layout-item' . ($value === $name ? ' active' : '') . '"><div class="layout-item-inside">';
+
+        $content .= '<div class="layout-image"><img src="' . $bundlePath . '/images/layouts/header/' . $name . '.png"></div>';
+        $content .= '<div class="layout-label">' . $label . '</div>';
+
+        $content .= '<div class="layout-button"><button data-value="' . $name . '">Aktivieren</button><div class="text">AKTIV</div></div>';
+
+        return $content . '</div></div>';
     }
 
 
