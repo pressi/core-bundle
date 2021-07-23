b2888d75420720eadadce2bb3aed485994781566
pressi <mail@stephanpressl.at>
2021-07-23 10:42:11 +0200

    Create CompanyModel.php

---
+++ src/Model/CompanyModel.php
@@ -0,0 +1,14 @@
+<?php
+
+
+namespace IIDO\CoreBundle\Model;
+
+
+use \Contao\Model;
+use HeimrichHannot\FieldpaletteBundle\Model\FieldPaletteModel;
+
+
+class CompanyModel extends FieldPaletteModel
+{
+    protected static $strTable = 'tl_iido_company';
+}
