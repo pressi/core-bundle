bc405a258ce5ea11a4353ba28fb7245bc61abb7a
pressi <mail@stephanpressl.at>
2021-09-29 17:25:50 +0200

    Update config.php

--- contao/config/config.php
+++ contao/config/config.php
@@ -4,16 +4,16 @@ use IIDO\CoreBundle\Widget\InputUnitWidget;
 
 
 
-/**
- * backend form fields
- */
-
-$GLOBALS['BE_FFL']['inputUnit'] = InputUnitWidget::class;
-
-
-
-/**
- * models
- */
-
-$GLOBALS['TL_MODELS']['tl_iido_company'] = \IIDO\CoreBundle\Model\CompanyModel::class;
+///**
+// * backend form fields
+// */
+//
+//$GLOBALS['BE_FFL']['inputUnit'] = InputUnitWidget::class;
+//
+//
+//
+///**
+// * models
+// */
+//
+//$GLOBALS['TL_MODELS']['tl_iido_company'] = \IIDO\CoreBundle\Model\CompanyModel::class;
